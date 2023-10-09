93be7907e2a2ce33084df89ab54e14298e6a63ec
Aaron Carter <aaron@arkaen.ai>
2023-10-09 16:25:33 -0500

    More refactoring
    Pushing hard for enough completion for basic working app sample

---
+++ src/DXSharp/DXFunctions.cs
@@ -0,0 +1,34 @@
+﻿using Windows.Win32.Graphics.Dxgi ;
+using DXSharp.DXGI ;
+using DXSharp.Windows ;
+using static Windows.Win32.PInvoke ;
+namespace DXSharp ;
+
+
+public static class DirectX {
+	internal static TFactory CreateFactory< TFactory >( Type factoryType )
+		where TFactory: class, IFactory< IDXGIFactory > {
+		ArgumentNullException.ThrowIfNull( factoryType, nameof(factoryType) ) ;
+		if ( !typeof( TFactory ).IsAssignableFrom( factoryType ) )
+			throw new ArgumentException( $"{nameof(factoryType)} must be assignable to TFactory" ) ;
+	}
+	
+	internal static TFactory CreateFactory0< TFactory >( ) 
+		where TFactory: class, IFactory< IDXGIFactory >, new() 
+	{
+		object? ppFactory    = null ;
+		HResult createResult = default ;
+		var     guid         = TFactory.InterfaceGUID ;
+		
+		unsafe { createResult = CreateDXGIFactory( &guid, out ppFactory ) ; }
+		if ( createResult.Failed || ppFactory is null )
+			throw new DirectXComError( "Failed to create DXGI Factory!" ) ;
+		
+		var factory = new TFactory( ) ;
+		factory.SetComPointer( new( (ppFactory as IDXGIFactory)! ) ) ;
+		return factory ;
+	}
+	
+	public static TFactory CreateFactory< TFactory >( )
+		where TFactory: class, IFactory< IDXGIFactory > => null ;
+}
---
+++ src/DXSharp/DXGI/Objects/OutputDuplication.cs
@@ -0,0 +1,6 @@
+﻿namespace DXSharp.DXGI ;
+
+public class OutputDuplication
+{
+	
+}
