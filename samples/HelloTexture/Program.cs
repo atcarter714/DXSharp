0cf1c33511afba26016649b8d0e0dd64954290fb
Aaron Carter <atcarter714@gmail.com>
2023-10-27 10:08:31 -0500

    Added new project for "Hello Texture" sample

---
+++ samples/HelloTexture/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing ;
+using DXSharp.Applications ;
+using DXSharp.Direct3D12.Debug ;
+
+
+AppSettings Settings = new( "DXSharp: Hello Texture",
+							(1280, 720),
+							new AppSettings.Style {
+								FontSize        = 13,
+								FontName        = "Consolas",
+								BackBufferColor = Color.Black,
+								ForegroundColor = Color.Black,
+								BackgroundColor = SystemColors.Window,
+							} ) ;
+
+#if DEBUG || DEBUG_COM
+var debug = Debug.CreateDebugLayer( ) ;
+debug.EnableDebugLayer( ) ;
+#endif
+
+IDXApp app = new DXWinformApp( Settings ) ;
+app.Initialize( ) ;
+app.Run( ) ;
