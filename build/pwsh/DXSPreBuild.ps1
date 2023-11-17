93d91b2937528145cbf3ffeac15651779e71eec1
Aaron Carter <atcarter714@gmail.com>
2023-11-16 20:47:50 -0600

    Updated gitignore and included DXSPreBuild.ps1 script ...

---
+++ build/pwsh/DXSPreBuild.ps1
@@ -0,0 +1,30 @@
+param (
+    [string]$Argument1,
+    [string]$Argument2,
+    [string]$Argument3="",
+)
+
+$scriptName = "DXSPreBuild.ps1"
+
+# Show startup message:
+Write-Host "DXS Pre-Build Tool: Startup script (DXSPreBuild.ps1) launched ..."
+
+
+
+# LogDeployment $args[0] $args[1] $args[2]
+function LogPreBuildEvent {
+  param( [string]$filepath, [string]$callerName, [string]$msg="" )
+  
+  # Get current date/time:
+  $datetime = Get-Date
+
+  # Create execution log text:
+  $filetext = "DXSharp pre-build tool executed at " + $datetime + " ..." + '\n'
+  
+  # Add message if provided:
+  if ($msg -ne "") {
+    $filetext = $filetext + "MSBuild Message :: " + '\n' + $msg + '\n'
+  }
+  
+  $filetext | Out-File -filepath $filepath -Append
+}
